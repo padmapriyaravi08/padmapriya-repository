b0541b061ff8543df3114d57fe1443240459245b
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2026-02-03 16:23:20 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("enter a number:");
+    int num=sc.nextInt();
+    int fact=1;
+    while(num>0){
+      fact=fact*num;
+      num --;
+    }
+    System.out.println("factorial is:"+fact);
+  }
+}
